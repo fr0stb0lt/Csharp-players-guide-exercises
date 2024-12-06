2456943b23714f2adaf2054540663e77bf1580ad
fr0stb0lt <97884166+fr0stb0lt@users.noreply.github.com>
2024-12-06 12:06:34 +0100

    Create ThePoint.cs
    Class exercise

---
+++ ThePoint.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Drawing;
+
+Point testcords = new(-4f, 0f);
+Console.WriteLine($"X: {testcords.Xcoordinate}, Y: {testcords.Ycoordinate}");
+
+
+public class Point
+{
+
+	private float xcoordinate;
+	private float ycoordinate;
+
+	
+
+	//Constructor
+	public Point(float x, float y)
+	{
+		this.xcoordinate = x;
+		this.ycoordinate = y;
+
+	}
+
+
+    public float Origin { get; set; }
+
+
+    public float Xcoordinate
+    {
+
+        get { return xcoordinate; }
+        set { xcoordinate = value; }
+
+    }
+
+    public float Ycoordinate
+
+    {
+        get { return ycoordinate; }
+        set { ycoordinate = value; }
+    }
+
+
+}
