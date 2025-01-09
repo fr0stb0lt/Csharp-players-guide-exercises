16b8da141cb7b87744e261251c38f35877e6013f
fr0stb0lt <97884166+fr0stb0lt@users.noreply.github.com>
2025-01-09 12:51:24 +0100

    Update TheLockedDoor.cs

--- TheLockedDoor.cs
+++ TheLockedDoor.cs
@@ -1,39 +1,3 @@
-
-/*
-Boss Battle The Locked Door 100 XP
-
-The fourth pedestal demands constructing a door class with a locking mechanism that requires a unique
-numeric code to unlock. You have done something similar before without using a class, but the locking
-mechanism is new.The door should only unlock if the passcode is the right one. The following statements
-describe how the door works.
-• An open door can always be closed.
-• A closed (but not locked) door can always be opened.
-• A closed door can always be locked.
-• A locked door can be unlocked, but a numeric passcode is needed, and the door will only unlock if
-the code supplied matches the door’s current passcode.
-• When a door is created, it must be given an initial passcode.
-• Additionally, you should be able to change the passcode by supplying the current code and a new
-one.The passcode should only change if the correct, current code is given.
-
-
-Objectives:
-[x] Define a Door class that can keep track of whether it is locked, open, or closed.
-
-[x] Make it so you can perform the four transitions defined above with methods.
-
-[x] Build a constructor that requires the starting numeric passcode.
-
-[] Build a method that will allow you to change the passcode for an existing door by supplying the
-current passcode and new passcode. Only change the passcode if the current passcode is correct.
-
-[] Make your main method ask the user for a starting passcode, then create a new Door instance.Allow
-the user to attempt the four transitions described above (open, close, lock, unlock) and change the
-code by typing in text commands.
-
-*/
-
-
-
 namespace theDoor;
 
 
@@ -56,6 +20,8 @@ class Program
             Console.WriteLine("1: Try to open the door:");
             Console.WriteLine("2: Close the door:");
             Console.WriteLine("3: Lock the door:");
+            Console.WriteLine("4: Unlock the door:");
+            Console.WriteLine("5: Change lock code:");
 
             string doorChoice = Console.ReadLine();
             door = doorChoice switch
@@ -122,6 +88,10 @@ class Door
                 CurrentDoorState = DoorState.Open;
                 Console.WriteLine("The door is now open");
                 break;
+            case DoorState.Unlocked:
+                CurrentDoorState = DoorState.Open;
+                Console.WriteLine("The door is now open");
+                break;
             case DoorState.Locked:
                 Console.WriteLine("The door is locked.");
 
@@ -166,10 +136,18 @@ class Door
     
     public Door UnLockDoor()
     {
-
-
-        CurrentDoorState = DoorState.Unlocked; 
-        Console.WriteLine("The door is now locked"); 
+        Console.WriteLine("Please enter the lock code:");
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        if(userInput == _lockCode)
+        {
+            CurrentDoorState = DoorState.Unlocked; 
+            Console.WriteLine("Lock code correct. Door unlocked.");
+        } else
+        {
+            Console.WriteLine("Lock code incorrect. Access denied.");
+        }
+        
+       
         return this;
         
     }
@@ -177,8 +155,18 @@ class Door
     public Door ChangeLockCode()
     {
         Console.WriteLine("Please enter the old lock code:");
-        var userInput = Convert.ToInt32(Console.ReadLine());
-        
+        int userInput = Convert.ToInt32(Console.ReadLine());
+        if (userInput == _lockCode)
+        {
+            Console.WriteLine("Lock code correct. Please enter the new lock code:");
+            var userCode = Convert.ToInt32(Console.ReadLine());
+            _lockCode = userCode;
+            
+        }
+        else
+        {
+            Console.WriteLine("Lock code incorrect.");
+        }
 
 
         return this;
